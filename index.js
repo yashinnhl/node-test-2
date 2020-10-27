04697565fac70928a8bd73ebc6f2659d2f49fd43
yashinnhl <yashin.nhl@gmail.com>
2020-10-26 23:11:08 -0400

    Added my project

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+let PORT = process.env.PORT || '8989';
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Wao your app is workinggggg");
+});
+
+app.get("/home", (req, res) => {
+  res.send("home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listen to port ${PORT}`);
+});
+
+// http.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('pedar sag');
+//   res.end();
+// }).listen(PORT);
+
+// console.log(`serve is listeting to portçc ${PORT}`);
